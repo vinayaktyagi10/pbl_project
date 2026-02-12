e822a197ef44142daa1e88ddbd40f7be6af5ef6a
Vinayak Tyagi <vinayaktyagi.ed@gmail.com>
2026-02-12 11:09:59 +0530

    Integrate portfolio webpage and presentation into unified deployment

--- global-top.vue
+++ global-top.vue
@@ -1,5 +1,8 @@
 <template>
-  <img src="/sdc_logo.jpg" class="sdc-logo" />
+  <img src="./sdc_logo.jpg" class="sdc-logo" />
+  <a href="../" class="back-home-link">
+    🏠 Home
+  </a>
 </template>
 
 <style>
@@ -9,5 +12,21 @@
   right: 1.5rem;
   width: 50px;
   z-index: 100;
+}
+.back-home-link {
+  position: absolute;
+  bottom: 1rem;
+  left: 1rem;
+  font-size: 0.8rem;
+  opacity: 0.5;
+  text-decoration: none;
+  color: inherit;
+  z-index: 100;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+}
+.back-home-link:hover {
+  opacity: 1;
 }
 </style>
